3cb09ce374c36311f5086d7e1c66aafa510b8f3b
aman <samanjot94@gmail.com>
2022-09-24 23:07:41 +0530

    new files

---
+++ script.js
@@ -0,0 +1,9 @@
+// var button=document.querySelector('#blogs');
+// var button2=document.querySelector('#btn1');
+// var div=document.querySelector('.backend');
+// var div2=document.querySelector('.prj-img');
+// button2.addEventListener('click',()=>div2.scrollIntoView({behavior:"smooth"}));
+// button.addEventListener('click',()=>div.scrollIntoView({behavior:"smooth"}));
+
+
+console.log("hello");
